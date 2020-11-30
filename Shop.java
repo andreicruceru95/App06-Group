57753bb3b2387f494f7d6be564faab8f5b3f90bc
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-11-30 22:46:46 +0000

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -7,6 +7,86 @@
  */
 public class Shop
 {
-    // instance variables - replace the example below with your own
+    private static final Potion POTION = new Potion(); 
+    private static final int POTION_PRICE = 5;
+    private static final int ATTACK_PRICE = 500;
+    private static final int SHIELD_PRICE = 500;
+    private static final int HEALTH_PRICE = 500;
+    private static final int ATTACK = 25;
+    private static final int SHIELD = 25;
+    private static final int HEALTH = 150;
     
+    
+    public void openShop()
+    {
+        String[] list = new String[]
+        {
+            "\tBuy Potion \t\t" + POTION_PRICE + " Gold",
+            "\tBuy Attack +" + ATTACK + "\t\t" + ATTACK_PRICE + " Gold",
+            "\tBuy Deffence +" + SHIELD + "\t\t" + SHIELD_PRICE + " Gold",            
+            "\tBuy Health +" + HEALTH + "\t\t" + HEALTH_PRICE + " Gold"            
+        };
+        
+        for(int i = 0; i < list.length; i++)
+        {
+            System.err.println(list[i]);
+        }
+        
+    }
+    
+    /**
+     * return potion price.
+     */
+    public int getPotionPrice()
+    {
+        return POTION_PRICE;    
+    }
+    
+    /**
+     * return atack price.
+     */
+    public int getAttackPrice()
+    {
+        return ATTACK_PRICE;    
+    }
+    
+    /**
+     * @return attack value
+     */
+    public int getAtackValue()
+    {
+        return ATTACK;
+    }
+    
+    /**
+     * return shield price.
+     */
+    public int getShieldPrice()
+    {
+        return SHIELD_PRICE;    
+    }
+    
+    /**
+     * @return shield value
+     */
+    public int getShieldValue()
+    {
+        return SHIELD;
+    }
+    
+    /**
+     * return health price.
+     */
+    public int getHealthPrice()
+    {
+        return HEALTH_PRICE;    
+    }
+    
+    /**
+     * @return health value
+     */
+    public int getHealthValue()
+    {
+        return HEALTH;
+    }
 }
