dfddaaefc2e8ed41a78988e2c874cd2023f5d516
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-12-09 14:44:48 +0000

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -5,88 +5,21 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Weapon
+public class Weapon extends Item
 {
-    public int attackPerLevel = 12;
     
-    private String name;
-    private int attackForce;
-    private int level;
-    private int displayLevel;
-    
-    /**
-     * constructor for weapon.
-     */
-    public Weapon()
+    public Weapon(String name, int baseStats, int enchantLevel, int bonusPerLevel)
     {
-        this.name = "Steel Sword";
-        this.attackForce = attackPerLevel;
-        this.level = 1;
-        this.displayLevel = 1;
-    }
-    
-    /**
-     * Increment a weapon level.
-     * Every 9 levels the name changes
-     */
-    public void enchance()
-    {
-        int evolution1 = 9;
-        int evolution2 = 18;
-        int evolution3 = 27;
-        int evolution4 = 36;
+        super(name, baseStats, enchantLevel, bonusPerLevel);
         
-        this.attackForce += attackPerLevel;
-        
-        this.level ++;
-        this.displayLevel ++;
-        if(level > evolution1)
-        {
-            this.name = "Silver Sword";
-            attackPerLevel = 15;
-            this.displayLevel = 1;
-        }
-        else if(level > evolution2)
-        {
-            this.name = "Full Moon Sword";
-            attackPerLevel = 25;
-            this.displayLevel = 1;
-        }
-        else if(level > evolution3)
-        {
-            this.name = "Dragon Sword";
-            attackPerLevel = 40;
-            this.displayLevel = 1;
-        }
-        else if(level > evolution4)
-        {
-            this.name = "Poison Sword";
-            attackPerLevel = 80;
-            this.displayLevel = 1;
-        }
     }
     
     /**
-     * @return attack value.
+     * Print the weapon's stats.
      */
-    public int getAttackValue()
+    public void print()
     {
-        return level * attackPerLevel;
+        System.out.println(displayName + " + " + displayLevel + "\tAttack: " + enchantStats);
     }
     
-    /**
-     * @return weapon attributes
-     */
-    public String getWeaponAttributes()
-    {
-        return "Weapon: " + name + " + " + displayLevel + " Attack: " + attackForce;
-    }
-    
-    /**
-     * @return attack per level
-     */
-    public int getAttackPerLevel()
-    {
-        return attackPerLevel;
-    }
 }
