9e404c48e9f05aca4d27035d2446fee87c120df7
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-12-10 17:40:45 +0000

    Create Bracelet.java

---
+++ Bracelet.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Bracelet here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Bracelet extends Item
+{
+    public Bracelet(String name, int baseStats, int enchantLevel, int bonusPerLevel)
+    {
+        super(name, baseStats, enchantLevel, bonusPerLevel);
+    }
+    
+    public void print()
+    {
+        System.out.println(displayName + " + " + displayLevel + "\tChance of evasion : " + enchantStats);
+    }
+}
