ddd201ba4819aea0d7f2983e401c464ccbae8bbe
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-12-10 14:47:32 +0000

    Create Display.java

---
+++ Display.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class Menu here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Display
+{
+    public static final char CLEAR = '\u000c';
+    private Commands command;
+    private Input reader = new Input();
+    
+    public void listOptions(String[] list)
+    {
+        
+        for(int i = 0; i < list.length; i++)
+        {
+            System.out.println(list[i]);
+        }
+        
+    }
+    
+    /**
+     * Display a given list as a story.
+     */
+    public void runStory(String[] part)
+    {
+        
+        for(int i = 0; i < part.length; i ++)
+        {
+            System.out.println(command.PRESS_ANY.getCommand());
+            reader.getAny();
+            
+            System.out.println(CLEAR);
+            
+            System.out.println(part[i]);
+        }
+        
+    }
+}
