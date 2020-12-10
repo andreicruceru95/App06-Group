ef7981035b19d9b2f11725f66eb8680ee5f803ce
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-12-10 15:20:48 +0000

    Update Interactions.java

--- Interactions.java
+++ Interactions.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 /**
  * Write a description of class Interactions here.
@@ -7,27 +8,30 @@
  */
 public class Interactions
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Interactions
-     */
+    public static final String DESSERT = "dessert";
+    public static final String SPIDER_CAVE = "spidercave";
+    
+    private HashMap<String, String> interactions = new HashMap<>();
+    
+    private Characters character;
+    
     public Interactions()
     {
-        // initialise instance variables
-        x = 0;
+        interactions.put(character.SHOP.getCharacter(), "There is a shop there, you can buy health potions and other things from there");
+        interactions.put(character.BLACKSMITH.getCharacter(),"There is a blacksmith there, you can ask him to improve your items for a price.\n" +
+                                "Ask him \"enchance armour\" or \"enchance weapon\" or \"enchance potion\".");
+        interactions.put(character.STABLE.getCharacter(), "There is a stable there, you soon be able to buy a horse from there");
+        interactions.put(character.GUARD.getCharacter(), "Town guard: \t\tHey, You! \n\t\tCome here. \n\tAre you that warrior " + 
+                                "everyone is talking about?\n\tIf you are that brave, why don't you go" + 
+                                "and kill some wolves for us?\n\tThey have been killing our people for a while.\n");
+        interactions.put(character.TELEPORT.getCharacter(), "There is a teleporter there, you can go to a different map.");    
+        interactions.put(DESSERT, "This was once a beautifull forrest. Now it's a dangerous death field. Be aware!");
+        interactions.put(SPIDER_CAVE, "A cave? You should be carefull.. Who knows what monsters lives in here.");
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    public boolean getInteraction(String object)
     {
-        // put your code here
-        return x + y;
+         System.out.println(interactions.get(object));
+         return true;
     }
 }
