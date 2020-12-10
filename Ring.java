133fcac9e49544a61a6d6822a708757c5516aa82
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-12-10 17:41:17 +0000

    Create Ring.java

---
+++ Ring.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Ring here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Ring extends Item
+{
+    
+    public Ring(String name, int baseStats, int enchantLevel, int bonusPerLevel)
+    {
+        super(name, baseStats, enchantLevel, bonusPerLevel);
+    }
+    
+    public void print()
+    {
+        System.out.println(displayName + " + " + displayLevel + "\tChance of double hit : " + enchantStats);
+    }
+}
