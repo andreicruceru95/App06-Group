2472d85492ef7c2a3456a513e262912d245f2be3
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-12-13 17:02:16 +0000

    Update Quest.java

--- Quest.java
+++ Quest.java
@@ -9,15 +9,17 @@ public class Quest
 {
     private String name;
     private String requirement;
+    private int requirementAmount;
     private String reward;
     private int amount;
     private boolean finished = false;
     private boolean started = false;
     
-    public Quest(String name, String requirement, String reward, int amount)
+    public Quest(String name, String requirement, int requirementAmount, String reward, int amount)
     {
         this.name = name;
         this.requirement = requirement;
+        this.requirementAmount = requirementAmount;
         this.reward = reward;
         this.amount = amount;
         
@@ -31,6 +33,11 @@ public class Quest
     public String getRequirement()
     {
         return requirement;
+    }
+    
+    public int getRequirementAmount()
+    {
+        return requirementAmount;
     }
     
     public String getReward()
@@ -61,6 +68,11 @@ public class Quest
     public boolean isFinished()
     {
         return finished;
+    }
+    
+    public String print()
+    {
+        return "\t" + name + "\n\t" + requirement + "\n\t" + "Reward:\t" + amount + " " + reward;
     }
     
 }
