866e49fa6656f6329a6b6d12e671f91979b30202
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2021-01-06 15:05:32 +0000

    Update Potion.java

--- Potion.java
+++ Potion.java
@@ -1,27 +1,37 @@
 import java.util.Random;
 
 /**
- * Write a description of class Potion here.
+ * Health Potion.
+ * Restores player's Health
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @authors Andrei Cruceru
+ * @version 1.0.12
  */
 public class Potion extends Item
 {
     int amount = 15;
     private static final Random random = new Random();
     
+    /**
+     * Initialise the potion.
+     */
     public Potion(String name, int baseStats, int enchantLevel, int bonusPerLevel)
     {
         super(name, baseStats, enchantLevel, bonusPerLevel);
     }
     
+    /**
+     * Print the potions stats.
+     */
     public void print()
     {
         System.out.println(displayName + "\t\t + " + displayLevel + "\tHeal: " + enchantStats +
                             "\tAmount " + amount);
     }
     
+    /**
+     * Print a random message each time a potion is used.
+     */
     public void printMessage()
     {
         int randomMessage = random.nextInt(5 - 1) + 1;
@@ -38,18 +48,27 @@ public class Potion extends Item
        
     }
     
+    /**
+     * Increase the amount of potions.
+     */
     public void increaseAmount(int amount)
     {
         this.amount += amount;
         
     }
     
+    /**
+     * Decrese the amount of potions.
+     */
     public void decreaseAmount()
     {
         amount --;
         
     }
     
+    /**
+     * @return the amount of potions.
+     */
     public int getAmount()
     {
         return amount;
