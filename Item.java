cc95bc17eee1edb7fbf6849ffa403f8d0d573fae
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2021-01-14 23:22:43 +0000

    Update Item.java

--- Item.java
+++ Item.java
@@ -25,16 +25,28 @@ public class Item
     protected int enchantStats;     
     protected int cost = 0; 
     protected int multiplier = 0;
+    protected int bonusPerAdvancement = 10;
+    protected boolean exists;
    
     /**
      * Initialising the item.
+     * @param name - item name.
+     * @param baseStats = the basic status of an item.
+     * @param enchantLevel = the enchantment level of the item at creation. 
+     * Usefull in the future where the player can gain random items each with a random enchantment level.
+     * @param bonusPerLevel = the rate of status upgrade for each level.
+     * @param cost = the cost of enchantment for each level.
+     * @param multiplier = the rate of cost changing.
+     * @param exists = visibility of the item(obtained/not obtained).
      */
-    public Item(String name, int baseStats, int enchantLevel, int bonusPerLevel, int cost, int multiplier)
+    public Item(String name, int baseStats, int enchantLevel, int bonusPerLevel, int cost, int multiplier, boolean exists)
     {
         this.bonusPerLevel = bonusPerLevel;
         this.cost = cost;
         this.multiplier = multiplier;
         this.name = name;
+        this.exists = exists;
+        
         displayName = EVO_NAME_1 + " " + name;
         
         this.enchantLevel = enchantLevel;
@@ -64,6 +76,7 @@ public class Item
         {
             enchantLevel ++;
             displayLevel ++;
+            cost += multiplier;
             
             enchantStats = (baseStats + ( enchantLevel * bonusPerLevel ));
             
@@ -85,6 +98,8 @@ public class Item
             
             displayName = EVO_NAME_2 + " " + name;
             
+            bonusPerLevel += bonusPerAdvancement;
+            
             return true;
         }
         else if(enchantLevel >= EVO_LEVEL_2)
@@ -93,6 +108,8 @@ public class Item
             
             displayName = EVO_NAME_3 + " " + name;
             
+            bonusPerLevel += bonusPerAdvancement;
+            
             return true;
         }
         else if(enchantLevel >= EVO_LEVEL_3)
@@ -100,6 +117,8 @@ public class Item
             displayLevel = enchantLevel % EVO_LEVEL_1;
             
             displayName = EVO_NAME_4 + " " + name;
+            
+            bonusPerLevel += bonusPerAdvancement;
             
             return true;
         }
@@ -116,11 +135,32 @@ public class Item
     }
     
     /**
+     * get next stats
+     */
+    public int getNextStats()
+    {
+        return enchantStats + bonusPerLevel;
+    }
+    
+    /**
      * @return an item's name.
      */
     protected String getName()
     {
         return name;
+    }
+    
+    /**
+     * Check if item exists
+     */
+    public boolean checkVisibility()
+    {
+        return exists;
+    }
+    
+    public void setVisibility()
+    {
+        exists = true;
     }
 }
 
