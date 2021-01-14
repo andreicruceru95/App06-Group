34a2334aa9c03eedc3c83a8254ab9877e36e0fb5
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2021-01-14 23:23:07 +0000

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -11,9 +11,9 @@ public class Weapon extends Item
     /**
      * Initialising the weapon.
      */
-    public Weapon(String name, int baseStats, int enchantLevel, int bonusPerLevel, int cost)
+    public Weapon(String name, int baseStats, int enchantLevel, int bonusPerLevel, int cost, int multiplier, boolean exists)
     {
-        super(name, baseStats, enchantLevel, bonusPerLevel, cost);
+        super(name, baseStats, enchantLevel, bonusPerLevel, cost, multiplier, exists);
         
     }
     
