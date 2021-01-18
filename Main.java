d2ea63732603632e791229188ec2fe6090c2c6ab
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2021-01-18 11:37:20 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 /**
  * This class contains the Main method that will run the game.
  *
@@ -7,6 +14,7 @@
 public class Main
 {
     private static final char CLEAR = '\u000c';
+    private Game game;
     
     /**
      * Run the game
@@ -15,7 +23,37 @@ public class Main
     {
         System.out.println(CLEAR);
         
-        Game game = new Game();
+        game = new Game();
+        
+        try
+        {
+            saveGame();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error");
+        }
+    }
+    
+    /**
+     * Save player's data
+     */
+    private void saveGame() throws Exception
+    {
+        final FileOutputStream FOS = new FileOutputStream("Game-data.txt");
+        final ObjectOutputStream OOS = new ObjectOutputStream(FOS);
+        
+        OOS.writeObject(game);
+        
+        OOS.close();
+        
+        final FileInputStream FIS = new FileInputStream("Game-data.txt");
+        final ObjectInputStream OIS = new ObjectInputStream(FIS);          
+        Game newGame = (Game) OIS.readObject();
+        
+        newGame.run();
+        
+        OIS.close();
     }
     
 }
